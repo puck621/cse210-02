9c1e3c28f1e2f607de375cb2d500faef3e667e07
puck621 <92011971+puck621@users.noreply.github.com>
2022-01-20 16:53:09 -0700

    Create deck.py

---
+++ game/deck.py
@@ -0,0 +1,20 @@
+import random
+from game.player import Player
+
+
+class Deck:
+    def __init__(self):
+
+    def input(self):
+        self.player.get_card()
+
+    def start_game(self):
+
+
+    def update(self):
+
+
+    def output(self):
+
+
+    def next_round(self):
