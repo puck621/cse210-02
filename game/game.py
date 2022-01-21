3dfbd61674880c119cb4fce651681bcf78a0f526
cub525 <49500036+cub525@users.noreply.github.com>
2022-01-20 17:37:25 -0700

    Emmett's changes to Game

--- game/game.py
+++ game/game.py
@@ -3,58 +3,68 @@ from game.player import Player
 
 
 class Game:
+    # This is a slightly more 'pythonic' way of doing num_players
+    def __init__(self):
+        self.players = [self.add_player() for i in range(int(input("How many players? ")))]
+        
+    def add_player(self):
+        player_name = input("What is player's name: ")
+        new_player = Player(player_name,300)
+        return new_player
 
-    players = []
-    h_or_l = ''
+    # End Emmett's Changes
 
-    def num_players():
+    def num_players(self):
         x = 1
         player_count = int(input("How many players? "))
         while player_count > 0:
             player_name = input(f"What is player {x}'s name: ")
             new_player = Player(player_name,300)
-            Game.players.append(new_player)
+            self.players.append(new_player)
             x +=1 
             player_count -= 1
 
-    def check_play(x, y):
+
+
+
+    def check_play(self, x, y):
         i = random.randint(1,13)
-        if Game.h_or_l.lower() == 'h':
+        if self.h_or_l.lower() == 'h':
             if x < i:
                 print(f"Next card was: {i}")
-                Game.players[y].score = Game.players[y].score + 100
-                print(f"{Game.players[y].name}'s score is {Game.players[y].score}")
+                self.players[y].score = self.players[y].score + 100
+                print(f"{self.players[y].name}'s score is {self.players[y].score}")
             else: 
                 print(f"Next card was: {i}")
-                Game.players[y].score = Game.players[y].score - 75
-                print(f"{Game.players[y].name}'s score is {Game.players[y].score}")
-        elif Game.h_or_l.lower() == 'l':
+                self.players[y].score = self.players[y].score - 75
+                print(f"{self.players[y].name}'s score is {self.players[y].score}")
+        elif self.h_or_l.lower() == 'l':
             if x > i:
                 print(f"Next card was: {i}")
-                Game.players[y].score = Game.players[y].score + 100
-                print(f"{Game.players[y].name}'s score is {Game.players[y].score}")
+                self.players[y].score = self.players[y].score + 100
+                print(f"{self.players[y].name}'s score is {self.players[y].score}")
             else:
                 print(f"Next card was: {i}")
-                Game.players[y].score = Game.players[y].score - 75
-                print(f"{Game.players[y].name}'s score is {Game.players[y].score}")
+                self.players[y].score = self.players[y].score - 75
+                print(f"{self.players[y].name}'s score is {self.players[y].score}")
 
-    def start_play():
+    def start_play(self):
         y = 0
-        for count in Game.players:
-            print(f"\n{Game.players[y].name}'s turn")
+        for count in self.players:
+            print(f"\n{self.players[y].name}'s turn")
             x = random.randint(1,13)
             print(f"The card is: {x}")
-            Game.h_or_l = input("Higher or Lower [h/l]: ")
-            Game.check_play(x, y)
+            self.h_or_l = input("Higher or Lower [h/l]: ")
+            self.check_play(x, y)
             y+=1
 
-    def main():
-        Game.num_players()
-        Game.start_play()
+    def main(self):
+        self.num_players()
+        self.start_play()
         while True: 
             again = input("Would you like to play again? [y/n]: ")
             if again.lower() == 'y':
-                Game.start_play()
+                self.start_play()
             else:
                 print("\nThanks for playing!")
                 break
