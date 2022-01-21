7542db9859744d088a9937d1972006ec2a442cb3
puck621 <92011971+puck621@users.noreply.github.com>
2022-01-20 17:13:53 -0700

    Update player.py

--- game/player.py
+++ game/player.py
@@ -1,9 +1,11 @@
 import random
 
-class Player:
-    def __init__(self):
+class Player: 
 
+    def __init__(self, name, score):
+        self.score = score
+        self.name = name
+        self.players = []
 
-    def get_card(self):
-
-    def points(self):
+    def get_score(self):
+        return self.score 
