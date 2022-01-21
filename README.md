63d8065f1ff341177084d293b125eb577ffd6465
cub525 <49500036+cub525@users.noreply.github.com>
2022-01-20 18:40:23 -0700

    Update everything collaboratively

--- README.md
+++ README.md
@@ -4,31 +4,32 @@ Overview
 Hilo is a game in which the player guesses if the next card drawn by the dealer will be higher or lower than the previous one. Points are won or lost based on whether or not the player guessed correctly.
 
 Rules
+---
 Hilo is played according to the following rules.
 
-The player starts the game with 300 points.
-Individual cards are represented as a number from 1 to 13.
-The current card is displayed.
-The player guesses if the next one will be higher or lower.
-The the next card is displayed.
-The player earns 100 points if they guessed correctly.
-The player loses 75 points if they guessed incorrectly.
-If a player reaches 0 points the game is over.
-If a player has more than 0 points they decide if they want to keep playing.
-If a player decides not to play again the game is over.
-Interface
+ - The player starts the game with 300 points.
+ - Individual cards are represented as a number from 1 to 13.
+ - The current card is displayed.
+ - The player guesses if the next one will be higher or lower.
+ - The the next card is displayed.
+ - The player earns 100 points if they guessed correctly.
+ - The player loses 75 points if they guessed incorrectly.
+ - If a player reaches 0 points the game is over.
+ - If a player has more than 0 points they decide if they want to keep playing.
+ - If a player decides not to play again the game is over.
+ - Interface
 
 
-Requirements
+Requirement
+---
 The program must also meet the following requirements.
 
-The program must include a README file.
-The program must include class and method comments.
-The program must have at least two classes.
-The program must remain true to game play described in the overview.
-Have Some Fun
-Have some fun by enhancing the game any way you like. A few ideas are as follows.
-
-Enhanced input validation.
-Enhanced game play and game over messages.
-Enhanced game display, e.g. card suits
+ - The program must include a README file.
+ - The program must include class and method comments.
+ - The program must have at least two classes.
+ - The program must remain true to game play described in the overview.
+ - Have Some Fun
+ - Have some fun by enhancing the game any way you like. A few ideas are as follows.
+   - Enhanced input validation.
+   - Enhanced game play and game over messages.
+   - Enhanced game display, e.g. card suits
--- game/game.py
+++ game/game.py
@@ -3,9 +3,15 @@ from game.player import Player
 
 
 class Game:
-    # This is a slightly more 'pythonic' way of doing num_players
+    """
+    Game class
+
+    attributes:
+    
+    """
     def __init__(self):
-        self.players = [self.add_player() for i in range(int(input("How many players? ")))]
+        self.players = [self.add_player() for _ in range(int(input("How many players? ")))]
+        self.h_or_l = ''
         
     def add_player(self):
         player_name = input("What is player's name: ")
@@ -13,48 +19,35 @@ class Game:
         return new_player
 
     # End Emmett's Changes
+    # def num_players(self):
+    #     x = 1
+    #     player_count = int(input("How many players? "))
+    #     while player_count > 0:
+    #         player_name = input(f"What is player {x}'s name: ")
+    #         new_player = Player(player_name,300)
+    #         self.players.append(new_player)
+    #         x +=1 
+    #         player_count -= 1
 
-    def num_players(self):
-        x = 1
-        player_count = int(input("How many players? "))
-        while player_count > 0:
-            player_name = input(f"What is player {x}'s name: ")
-            new_player = Player(player_name,300)
-            self.players.append(new_player)
-            x +=1 
-            player_count -= 1
 
+    # def check_play(self, x, player):
+    #     i = random.randint(1,13)
+    #     if (self.h_or_l.lower() == 'h' and x < i) or (self.h_or_l.lower() == 'l' and x > i): 
+    #         print(f"Next card was: {i}")
+    #         player.score = player.score + 100
+    #         print(f"{player.name}'s score is {player.score}")
+    #     else: 
+    #         print(f"Next card was: {i}")
+    #         player.score = player.score - 75
+    #         print(f"{player.name}'s score is {player.score}")
 
-    def check_play(self, x, y):
-        i = random.randint(1,13)
-        if self.h_or_l.lower() == 'h':
-            if x < i:
-                print(f"Next card was: {i}")
-                self.players[y].score = self.players[y].score + 100
-                print(f"{self.players[y].name}'s score is {self.players[y].score}")
-            else: 
-                print(f"Next card was: {i}")
-                self.players[y].score = self.players[y].score - 75
-                print(f"{self.players[y].name}'s score is {self.players[y].score}")
-        elif self.h_or_l.lower() == 'l':
-            if x > i:
-                print(f"Next card was: {i}")
-                self.players[y].score = self.players[y].score + 100
-                print(f"{self.players[y].name}'s score is {self.players[y].score}")
-            else:
-                print(f"Next card was: {i}")
-                self.players[y].score = self.players[y].score - 75
-                print(f"{self.players[y].name}'s score is {self.players[y].score}")
 
     # For in loops can be used to iterate over a list
     def start_play(self):
         for player in self.players:
             print(f"\n{player.name}'s turn")
-            x = random.randint(1,13)
-            print(f"The card is: {x}")
-            self.h_or_l = input("Higher or Lower [h/l]: ")
-            self.check_play(x, y)
-            y+=1
+            # Finley's code
+            player.play()
 
     def main(self):
         # self.num_players()
